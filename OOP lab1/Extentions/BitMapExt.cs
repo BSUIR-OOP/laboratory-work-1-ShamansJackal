ccd4940f663b794c0a094ff3d7f18cede96e12c8
p.horovenko <p.horovenko@LINKER-W3>
2022-03-15 13:24:41 +0300

    add drawing methods

---
+++ OOP lab1/Extentions/BitMapExt.cs
@@ -0,0 +1,32 @@
+﻿using OOP_lab1.Structs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media.Imaging;
+
+namespace OOP_lab1.Extentions
+{
+    public static class BitMapExt
+    {
+        private static void AddPixel(this WriteableBitmap wb, PixelColor color, int x, int y){
+            Int32Rect rect = new(x, y, 1, 1);
+            int stride = (wb.PixelWidth * wb.Format.BitsPerPixel) / 8;
+            wb.WritePixels(rect, color.Core, stride, 0);
+        }
+
+        public static void AddRect(this WriteableBitmap wb, PixelColor color, int x, int y, int size)
+        {
+            int beg_x = x-size < 0 ? 0 : x - size;
+            int beg_y = y-size < 0 ? 0 : y - size;
+            int end_x = x+size > wb.PixelWidth ? wb.PixelWidth : x + size;
+            int end_y = y+size > wb.PixelHeight ? wb.PixelHeight : y + size;
+
+            for (int i = beg_x; i < end_x; i++)
+                for (int j = beg_y; j < end_y; j++)
+                    wb.AddPixel(color, i, j);
+        }
+    }
+}
