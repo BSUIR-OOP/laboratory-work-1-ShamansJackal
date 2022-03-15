1d718b9df2bfec04b420ad5d2fed4e76d95d8b5e
p.horovenko <p.horovenko@LINKER-W3>
2022-03-15 13:32:13 +0300

    add shapes prototypes

---
+++ OOP lab1/Shapes/BaseShape.cs
@@ -0,0 +1,20 @@
+﻿using OOP_lab1.Structs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_lab1.Shapes
+{
+    public abstract class BaseShape
+    {
+        public PixelColor color;
+        public List<BezierCurve> Curves { get; protected set; }
+
+        public virtual void SetSize(int x1, int y1, int x2, int y2)
+        {
+            Curves = new List<BezierCurve>();
+        }
+    }
+}
